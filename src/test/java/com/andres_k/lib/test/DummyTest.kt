57dc1b51ba6baf1087e90734433fcb8c58fb8e8d
Draym <kevin.draym@gmail.com>
2021-07-02 15:19:22 +0800

    check that CD is working fine, should not try to deploy

---
+++ src/test/java/com/andres_k/lib/test/DummyTest.kt
@@ -0,0 +1,26 @@
+package com.andres_k.lib.test
+
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+/**
+ * Created on 2021/07/02.
+ *
+ * @author Kevin Andres
+ */
+class DummyTest {
+    @BeforeEach
+    fun setUp() {
+    }
+
+    @AfterEach
+    fun tearDown() {
+    }
+
+    @Test
+    fun drawText() {
+        assertEquals(1, 2)
+    }
+}
